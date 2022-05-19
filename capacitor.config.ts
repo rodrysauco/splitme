bf42cb386cf15bd33871462502c717a35c5e8004
Rodrigo Sauco <rodrigo.sauco@globallogic.com>
2022-05-19 11:57:18 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'splitme',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
